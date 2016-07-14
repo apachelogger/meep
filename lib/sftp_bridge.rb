70b25c305e48337a19d1bb90bc9899bd39c9f477
Harald Sitter <sitter@kde.org>
2016-07-14 13:46:10 +0200

    m

--- lib/sftp_bridge.rb
+++ lib/sftp_bridge.rb
@@ -71,7 +71,6 @@ class SFTPBridge
           @port = server.listeners[0].addr[1]
           server.mount('/', SFTPBridgeServlet)
           trap 'INT' do server.shutdown end
-            trap 'KILL' do server.shutdown end
               trap 'TERM' do server.shutdown end
           server.start
         end
